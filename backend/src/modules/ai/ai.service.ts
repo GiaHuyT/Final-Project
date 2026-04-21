eb416b2382d5cd444fdaeb33211939891032cdd1
GiaHuyT <giahuy31122004@gmail.com>
2026-04-21 23:51:22 +0700

    chore(backend): trigger ts server reload

--- backend/src/modules/ai/ai.service.ts
+++ backend/src/modules/ai/ai.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'prisma/prisma.service';
 import OpenAI from 'openai';
